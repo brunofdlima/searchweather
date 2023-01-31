a27e05d305fd39817a3ac37dafb259c0d7e22845
Bruno Ferreira <brunofor14@gmail.com>
2023-01-31 11:42:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import { GoSearch } from "react-icons/go";
+import { WiStrongWind, WiHumidity } from "react-icons/wi";
+import { api } from "./services";
+import {
+  Content,
+  Form,
+  Response,
+  ResponseTitle,
+  ResponseTemp,
+  Before,
+  Left,
+  Equal,
+  Temp,
+  Temp1,
+  Title,
+  Button,
+  Input,
+} from "./styles";
+
+const App = () => {
+  const [cidade, setCidade] = useState("");
+  const [previsao, setPrevisao] = useState<any>(null);
+
+  const handleChange = (event: any) => {
+    setCidade(event.target.value);
+  };
+
+  const handleSearch = async (e: any) => {
+    e.preventDefault();
+    await api
+      .get("", {
+        params: {
+          q: cidade,
+          appid: "9e869bd5113c9de4ddc44bc7c60bb738",
+          units: "metric",
+          lang: "pt_br",
+        },
+      })
+      .then((data) => {
+        setPrevisao(data.data);
+      });
+  };
+
+  const mystyle = {
+    height: "4.375rem",
+    width: "4.375rem",
+  };
+
+  return (
+    <Content>
+      <Form>
+        <Before>
+          <Title>Previsão do tempo para:</Title>
+          <div>
+            <Input
+              classname="searchCity"
+              value={cidade}
+              onChange={handleChange}
+            />
+            <Button onClick={handleSearch}>
+              <GoSearch style={{margin: "5px 1px 0 0"}}/>
+            </Button>
+          </div>
+        </Before>
+
+        <Response>
+          {previsao ? (
+            <div>
+              <ResponseTitle>{previsao.weather[0].description}</ResponseTitle>
+              <ResponseTemp>
+                <Left>
+                  <img
+                    src={`http://openweathermap.org/img/wn/${previsao.weather[0].icon}.png`}
+                    alt="City Weather Icon"
+                  />
+                  <Temp>
+                    <Temp1>{previsao.main.temp + "°c"}</Temp1>
+                    <div>Sensação de {previsao.main.feels_like + "°c"}</div>
+                  </Temp>
+                </Left>
+                <Equal>
+                  <WiHumidity style={mystyle} />
+                  <div>Umidade de {previsao.main.humidity} %</div>
+                </Equal>
+                <Equal>
+                  <WiStrongWind style={mystyle} />
+                  <div style={{ marginRight: "0.625rem" }}>
+                    Ventos de {previsao.wind.speed} Km/h
+                  </div>
+                </Equal>
+              </ResponseTemp>
+            </div>
+          ) : null}
+        </Response>
+      </Form>
+    </Content>
+  );
+};
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+import img from './pictures/backtemp.jpg';
+
+export const Content = styled.div`
+background-image: url(${img});
+height: 100vh;
+background-size: cover;
+font-family: 'Roboto Slab', serif;
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+export const Form = styled.div`
+width: 50rem;
+background-color: #85aab0;
+font-family: 'Roboto Slab', serif;
+border-radius: 0.625rem;
+align-items: center;
+`;
+
+export const Before = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`;
+
+export const Title = styled.h1`
+margin: 0.625rem 0.625rem;
+font-size: 1.6rem;
+font-weight: bold;
+justify-content: center;
+`;
+
+export const Input = styled.input`
+width: 15.5rem;
+height: 2.188rem;
+margin-left: 0.625rem;
+border: none;
+border-radius: 0.625rem;
+`;
+
+export const Button = styled.button`
+width: 2.188rem;
+height: 2.188rem;
+margin: 0.625rem 0;
+cursor: pointer;
+margin-left: 0.313rem;
+border: none;
+border-radius: 3.125rem;
+`;
+
+export const Response = styled.div`
+margin: 1.25rem 10px;
+`;
+
+export const ResponseTitle = styled.div`
+font-weight: bold;
+text-transform: uppercase;
+font-size: 1.25rem;
+margin: 1.25rem;
+`;
+
+export const ResponseTemp = styled.div`
+display: flex;
+justify-content: space-between;
+@media (max-width: 680px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const Left = styled.div`
+display: flex;
+margin-top: 0.2rem;
+`;
+
+export const Equal = styled.div`
+font-weight: bold;
+align-items: start;
+display: flex;
+align-items: center;
+justify-content: space-between;
+@media (max-width: 680px) {
+  margin-top: 30px;
+}
+`;
+
+export const Temp = styled.div`
+display: flex;
+flex-direction: column;
+margin-top: 0.25rem;
+`;
+
+export const Temp1 = styled.div`
+font-weight: bold;
+font-size: 2.5rem;
+`;
